0e9b8dd6ee5f5a4b63aa8f9addc65099e7984e2c
Tykunal <kunal2021CSAI065@abesit.edu.in>
2024-07-26 17:06:51 +0530

    Adding numpy assignment

---
+++ pythonAdvanced/numpy/ilt1Lab.py
@@ -0,0 +1,43 @@
+import numpy as np
+# 1.Convert the list to numpy and display
+
+# l = [1,2,3,4]
+# arr = np.array(l)
+# print(arr)
+
+# ------------------------------------------------------------------
+
+# 2. Perform the following operations
+# list = [1,2,3,4,5]
+# arr = np.array(list)
+
+# # Array Printing
+# print(f"Printing original array: {arr}")
+# # 1st and last element
+# size = len(arr)-1
+# print(f"First Element in arr: {arr[0]} and last element in arr: {arr[size]}")
+# # Mutiply each Element with 2
+# print(f"Multiplying each element of arr with 2: {arr*2}")
+
+# ------------------------------------------------------------------
+
+# 3.Create array of 10 zeros , 10ones and 10fives
+
+# zeroArr = np.zeros(10)
+# print(f"Array of 10 elements that are zeros: {zeroArr}")
+
+# oneArr = np.ones(10)
+# print(f"Array of 10 elements that are ones: {oneArr}")
+
+# fiveArr = np.full((10),5, dtype=float)
+# print(f"Array of 10 five: {fiveArr}")
+
+# ------------------------------------------------------------------
+
+# create a 3*3 matrix with values ranging from 2 to 10.
+
+# arr = np.arange(2,11).reshape(3,-1)
+# print(arr)
+
+# ------------------------------------------------------------------
+
